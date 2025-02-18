2db41518ed5eb27a8429cf02660884467796c8e1
Noaman <mn0359948@gmail.com>
2025-02-18 23:53:35 +0200

    Task Two

---
+++ Task Two MVC/Task Two MVC/Controllers/StudentController .cs
@@ -0,0 +1,61 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Task_Two_MVC.Data;
+using Task_Two_MVC.Entities;
+
+namespace Task_Two_MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public StudentController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Show All
+        [HttpGet]
+        // /student/ShowAll
+        public IActionResult ShowAll()
+        {
+            var std = _context.Students
+                .Include(s => s.Department)
+                .ToList();
+
+            return View(std);
+
+        }
+        #endregion
+
+
+
+
+        #region Show By Id
+        [HttpGet]
+        //[Route("Student/ShowById/{id}")]
+        public  IActionResult ShowById(int id)
+        {
+            var student =  _context.Students
+                .Include(s => s.Department)
+                .Include(s => s.StuCrsRes)
+                    .ThenInclude(cr => cr.Course)
+                .FirstOrDefault(s => s.Id == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+        #endregion
+
+    }
+}
