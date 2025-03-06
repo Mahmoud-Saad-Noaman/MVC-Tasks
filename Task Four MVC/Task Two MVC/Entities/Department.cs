3063d01f406aed7c2bdc0e1367a67646d6665604
Noaman <mn0359948@gmail.com>
2025-03-06 16:54:37 +0200

    Upload Task Four

---
+++ Task Four MVC/Task Two MVC/Entities/Department.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task_Two_MVC.Entities
+{
+    public class Department
+    {
+        #region Properties
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string MgrName { get; set; }
+
+
+        public List<Teacher> Teachers { get; set; } = new List<Teacher>();
+        public List<Student> Students { get; set; } = new List<Student>();
+        public List<Course> Courses { get; set; } = new List<Course>();
+        #endregion
+    }
+}
